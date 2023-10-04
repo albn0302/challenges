1ef87b26384ba5f03e30cbae77662314b08b0ca2
albn0302 <albin.johansson0302@gmail.com>
2023-10-04 08:51:45 +0300

    javascript-challenges

---
+++ challenge-01.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4, 5];
+
+function switchArr() {
+    arr.slice();
+    console.log(arr);
+}
---
+++ challenge-02.js
@@ -0,0 +1,8 @@
+import readline from "read-console-input";
+
+const name = readline("What's your name: ");
+console.log("Hello " + name);
+
+const a = Number(readline("Number 1: "));
+const b = Number(readline("Number 2: "));
+console.log(a + b);
